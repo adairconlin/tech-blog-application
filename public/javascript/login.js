4035ca2d94ace1d231b7d79fe52922e43ef0e023
adairconlin <adair.conlin@gmail.com>
2022-01-13 13:07:14 -0500

    Add user signup

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -19,4 +19,27 @@ async function userLogin(event) {
     }
 };
 
+async function userSignup(event) {
+    event.preventDefault();
+
+    const username = document.querySelector("#username-signup").value.trim();
+    const email = document.querySelector("#email-signup").value.trim();
+    const password = document.querySelector("#password-signup").value.trim();
+
+    if(username && email && password) {
+        const response = await fetch("/api/users", {
+            method: "post",
+            body: JSON.stringify({ username, email, password }),
+            headers: { "Content-Type": "application/json"}
+        })
+
+        if(response.ok) {
+            console.log("success");
+        } else {
+            alert(response.statusText);
+        }
+    }
+}
+
 document.querySelector(".login-form").addEventListener("submit", userLogin);
+document.querySelector(".signup-form").addEventListener("submit", userSignup);
