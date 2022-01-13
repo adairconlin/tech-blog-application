b89b142905d3e61ef012dd80410e905810d18b17
adairconlin <adair.conlin@gmail.com>
2022-01-13 12:06:47 -0500

    Begin front end

--- controllers/index.js
+++ controllers/index.js
@@ -3,7 +3,7 @@ const apiRoutes = require("./api");
 const homeRoutes = require("./home-routes");
 // const dashboardRoutes = require("./dashboard-routes");
 
-// router.use("/", homeRoutes);
+router.use("/", homeRoutes);
 router.use("/api", apiRoutes);
 // router.use("/dashboard", dashboardRoutes);
 
